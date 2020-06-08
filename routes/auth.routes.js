f6b05c4609633fa57a283ed48935acc7cefb862e
Raphael Etim <raphaelumoh@gmail.com>
2020-06-08 14:52:55 +0100

    add routes for authentication

---
+++ routes/auth.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const authController = require('../controllers/auth.controller');
+
+const router = express.Router();
+
+router.route('/auth/signup').post(authController.signup)
+router.route('/auth/login').post(authController.login);
+router.route('/auth/signout').get(authController.signout);
+
+module.exports = router;
