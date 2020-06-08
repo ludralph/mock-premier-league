04f9a3c6773acc854d0f2fdf6cd16717d1de708a
Raphael Etim <raphaelumoh@gmail.com>
2020-06-08 16:31:40 +0100

    format server and config file

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const app = require('./app')
+const app = require('./app');
 const dbConnection = require('./database');
 const config = require('./config/config');
 
 dbConnection();
 
 app.listen(config.port, () => {
-    console.log('App started....')
+  console.log('App started....');
 });
