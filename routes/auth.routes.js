f4845872b93126e8b9fd47ab274239de1a8d6d27
Raphael Etim <raphaelumoh@gmail.com>
2020-06-08 16:28:03 +0100

    update auth route file

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,7 +3,7 @@ const authController = require('../controllers/auth.controller');
 
 const router = express.Router();
 
-router.route('/auth/signup').post(authController.signup)
+router.route('/auth/signup').post(authController.signup);
 router.route('/auth/login').post(authController.login);
 router.route('/auth/signout').get(authController.signout);
 
