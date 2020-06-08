a095659a9acccd4b4cc709d4669ec9c703b67bb6
Raphael Etim <raphaelumoh@gmail.com>
2020-06-08 16:28:33 +0100

    add generic error handler

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
 const app = express();
+const authRoutes = require('./routes/auth.routes');
 
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
 
+app.use('/api/v1', authRoutes);
+app.get('/', (req, res) => {
+  res.send({
+    message: 'Welcome to the Mock Premier League API',
+  });
+});
 
-app.get('/', (req, res) =>{
-    res.send('Hello')
-})
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({
+      error: `${err.name}: ${err.message}`,
+    });
+  } else if (err) {
+    res.status(400).json({
+      error: `${err.name}: ${err.message}`,
+    });
+  }
+});
 
 module.exports = app;
-
