f7af5b637e6a5707494b10ca618d2a73712d6007
Raphael Etim <raphaelumoh@gmail.com>
2020-06-11 09:03:46 +0100

    re-implement logic for comparing two teams

--- helpers/utils.js
+++ helpers/utils.js
@@ -2,6 +2,15 @@ const escapeRegex = (text) => {
 	return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
 };
 
+const compareTwoTeams = (teamA, teamB) => {
+	if (teamA.length !== teamB.length) {
+		return false;
+   }
+   return teamA.localeCompare(teamB) === 0;
+};
+
+
 module.exports = {
-    escapeRegex
+	escapeRegex,
+	compareTwoTeams
 }
